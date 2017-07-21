a19342d704b70c31fd2cec002e9d874149e6b65b
Bhanu Shankar Pottipareddy <bhanushankar422@gmail.com>
2017-07-21 00:38:56 -0500

    updated the bot to havea postback 2

--- index.js
+++ index.js
@@ -193,5 +193,5 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, "Postback called by Bhanu");
 }
